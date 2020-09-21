fd4956d974ef501b38e7ea1b05dfbf8a88df6950
pstgtom <pstgtom@gmail.com>
2020-09-21 17:15:45 +0900

    ExcelWB部分を削除した。

--- 住所録リスト.cs
+++ 住所録リスト.cs
@@ -1,6 +1,4 @@
-﻿//using Microsoft.Office.Core;
-//using Microsoft.Office.Interop.Excel;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -13,21 +11,12 @@ using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-//using Excel = Microsoft.Office.Interop.Excel;
+using Npgsql;
 
 namespace 口酒井農業水利組合郵送会員住所録
 {
     public partial class 住所録リストForm : Form
     {
-        //住所録ファイルのパス
-        //public string path = (@"D:\develope\口酒井\住所録\");
-        public string 水利関係住所録WB = (@"C:\dropbox\住所録\水利関係住所録.xlsx");
-        //public string 水利関係住所録WB = ("水利関係住所録.xlsx");
-
-        //住所録呼出
-        public dynamic xlApp;
-        public dynamic xlBooks;
-        public dynamic xlBook;
 
         //修正後のデータ受け取り配列変数
         住所氏名編集Form fs;
@@ -40,59 +29,18 @@ namespace 口酒井農業水利組合郵送会員住所録
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            //水利関係住所録WBが開かれてないかのチェック
-            try
-            {
-                var FA = File.AppendText(水利関係住所録WB);
-                FA.Close();
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(水利関係住所録WB + "は既に開いています。閉じてからやり直してください。");
-                Close();
-                return;
-            }
 
             Text = "郵送住所録フォーム　" + "Ver.1.02" + "　　　　　　　定型長３封筒に印刷してください。";
 
-            //住所録呼出
-            //xlApp = new Excel.Application();
-            xlApp = Activator.CreateInstance(Type.GetTypeFromProgID("Excel.Application"));
-            xlBooks = xlApp.Workbooks;
-            xlBook = xlBooks.Open(水利関係住所録WB);
-
             ListView1呼出();
 
-            // シートを選択
-            var sashidasinin = xlBook.Sheets["差出人"];
-
-            // セルの領域を選択
-            var 差出人 = sashidasinin.Range["A2", "B2"];
-
-            // 選択した領域の値をメモリー上に格納
-            object[,] Sender = 差出人.Value;
 
             // 選択した領域の値をメモリー上に格納
             差出人Box.Text = (string)Sender[1, 1];
             差出人住所Box.Text = (string)Sender[1, 2];
 
 
-            // 使用したCOMオブジェクトを解放
-            System.Runtime.InteropServices.Marshal.ReleaseComObject(差出人);
-            System.Runtime.InteropServices.Marshal.ReleaseComObject(sashidasinin);
-
-            // Excelのクローズ
-            //xlBook.Saved = true;
-            //xlBook.Close();
-            //xlBooks.Close();
-            //xlApp.Quit();
-
             MessageBox.Show("定型長３封筒に印刷してください。");
-
-            //// 使用したCOMオブジェクトを解放その２
-            //System.Runtime.InteropServices.Marshal.ReleaseComObject((object)xlBook);
-            //System.Runtime.InteropServices.Marshal.ReleaseComObject((object)xlBooks);
-            //System.Runtime.InteropServices.Marshal.ReleaseComObject((object)xlApp);
 
         }
 
@@ -112,16 +60,6 @@ namespace 口酒井農業水利組合郵送会員住所録
             listView1.Columns.Add("住　所", 300);
             listView1.Columns.Add("分類", 75);
 
-            // シートを選択
-            var sheet = xlBook.Sheets["口酒井名簿"];
-
-            // セルの領域を選択
-            //var TableRange = sheet.Range["A2", "E47"];
-            var TableRange = sheet.Range["A2"].CurrentRegion;
-
-            // 選択した領域の値をメモリー上に格納
-            object[,] values = TableRange.Value;
-
             // 配列アクセスができるので、それぞれをリストビューに追加
 
             ListViewItem lvi;
@@ -134,9 +72,6 @@ namespace 口酒井農業水利組合郵送会員住所録
                 lvi.SubItems.Add((string)values[i, 4]);
                 lvi.SubItems.Add((string)values[i, 5]);
             }
-
-            System.Runtime.InteropServices.Marshal.ReleaseComObject(TableRange);
-            System.Runtime.InteropServices.Marshal.ReleaseComObject(sheet);
 
         }
 
@@ -171,15 +106,6 @@ namespace 口酒井農業水利組合郵送会員住所録
         {
             string 氏名, 郵便番号, 住所;
 
-            //xlBook = xlBooks.Open(水利関係住所録WB);
-
-            // シートを選択
-            var 口酒井名簿 = xlBook.Sheets["口酒井名簿"];
-
-            // セルの領域を選択
-            var TableRange = 口酒井名簿.Range["A2"].CurrentRegion;
-
-            // 選択した領域の値をメモリー上に格納
             object[,] values = TableRange.Value;
 
 
@@ -196,10 +122,6 @@ namespace 口酒井農業水利組合郵送会員住所録
                     宛名印刷(氏名, 郵便番号, 住所);
                 }
             }
-
-            // 使用したCOMオブジェクトを解放
-            System.Runtime.InteropServices.Marshal.ReleaseComObject(TableRange);
-            System.Runtime.InteropServices.Marshal.ReleaseComObject(口酒井名簿);
 
         }
 
@@ -240,25 +162,21 @@ namespace 口酒井農業水利組合郵送会員住所録
 
         private void 初期化()
         {
-            //xlBook = xlBooks.Open(水利関係住所録WB);
 
-            ////シートを選択
-            var 宛名面 = xlBook.Sheets["宛名面"];
-
-            宛名面.Shapes(1).Name = "宛先郵便番号";
-            宛名面.Shapes(2).Name = "宛先住所";
-            宛名面.Shapes(3).Name = "宛先氏名1";
-            宛名面.Shapes(4).Name = "宛先氏名2";
-            宛名面.Shapes(5).Name = "差出人氏名";
-            宛名面.Shapes(6).Name = "差出人住所";
+            //宛名面.Shapes(1).Name = "宛先郵便番号";
+            //宛名面.Shapes(2).Name = "宛先住所";
+            //宛名面.Shapes(3).Name = "宛先氏名1";
+            //宛名面.Shapes(4).Name = "宛先氏名2";
+            //宛名面.Shapes(5).Name = "差出人氏名";
+            //宛名面.Shapes(6).Name = "差出人住所";
 
 
-            宛名面.Shapes("宛先郵便番号").TextFrame.Characters.Text = "";      //宛先郵便番号
-            宛名面.Shapes("宛先住所").TextFrame.Characters.Text = "";          //宛先住所
-            宛名面.Shapes("宛先氏名1").TextFrame.Characters.Text = "";         //宛先氏名1
-            宛名面.Shapes("宛先氏名2").TextFrame.Characters.Text = "";         //宛先氏名2
-            宛名面.Shapes("差出人氏名").TextFrame.Characters.Text = "";       //差出人氏名
-            宛名面.Shapes("差出人住所").TextFrame.Characters.Text = "";       //差出人住所
+            //宛名面.Shapes("宛先郵便番号").TextFrame.Characters.Text = "";      //宛先郵便番号
+            //宛名面.Shapes("宛先住所").TextFrame.Characters.Text = "";          //宛先住所
+            //宛名面.Shapes("宛先氏名1").TextFrame.Characters.Text = "";         //宛先氏名1
+            //宛名面.Shapes("宛先氏名2").TextFrame.Characters.Text = "";         //宛先氏名2
+            //宛名面.Shapes("差出人氏名").TextFrame.Characters.Text = "";       //差出人氏名
+            //宛名面.Shapes("差出人住所").TextFrame.Characters.Text = "";       //差出人住所
 
 
         }
@@ -267,11 +185,6 @@ namespace 口酒井農業水利組合郵送会員住所録
         private void 宛名印刷(string 氏名, string 郵便番号, string 住所)
         {
             初期化();
-
-            //xlBook = xlBooks.Open(水利関係住所録WB);
-
-            ////シートを選択
-            var 宛名面 = xlBook.Sheets["宛名面"];
 
             //郵便番号がnullの場合、0000でnull合体演算をする
             string fourZeros = "000-0000";
@@ -303,18 +216,8 @@ namespace 口酒井農業水利組合郵送会員住所録
             郵便番号 = 郵便番号.Replace('9', '９');
             //MessageBox.Show(郵便番号);
 
-            //宛名面2.Range["L1"].value = 郵便番号.Substring(0, 1);
-            //宛名面2.Range["E1"].value = 郵便番号.Substring(1, 1);
-            //宛名面2.Range["F1"].value = 郵便番号.Substring(2, 1);
-            //宛名面2.Range["G1"].value = 郵便番号.Substring(3, 1);
-            //宛名面2.Range["I1"].value = 郵便番号.Substring(4, 1);
-            //宛名面2.Range["J1"].value = 郵便番号.Substring(5, 1);
-            //宛名面2.Range["K1"].value = 郵便番号.Substring(6, 1);
-
             住所.Replace("－", "ー");
             住所.Replace("-", "ー");
-
-            //宛名面2.Range["J3"].value = 住所;
 
             氏名 = 氏名.Replace("　", "");
             氏名 = 氏名.Replace(" ", "");
@@ -340,26 +243,16 @@ namespace 口酒井農業水利組合郵送会員住所録
                 氏名 = 氏名 + "様";
             }
 
-            //宛名面2.Range["D3"].value = 氏名;
 
+            //宛名面.Shapes("宛先郵便番号").TextFrame.Characters.Text = 郵便番号;                           //宛先郵便番号
+            //宛名面.Shapes("宛先住所").TextFrame.Characters.Text = 住所;                  //宛先住所
+            //宛名面.Shapes("宛先氏名1").TextFrame.Characters.Text = 氏名;                             //宛先氏名1
+            //宛名面.Shapes("宛先氏名2").TextFrame.Characters.Text = 氏名2;                                           //宛先氏名2
+            //宛名面.Shapes("差出人氏名").TextFrame.Characters.Text = 差出人氏名;                         //差出人氏名
+            //宛名面.Shapes("差出人住所").TextFrame.Characters.Text = 差出人住所;       //差出人住所
 
-            宛名面.Shapes("宛先郵便番号").TextFrame.Characters.Text = 郵便番号;                           //宛先郵便番号
-            宛名面.Shapes("宛先住所").TextFrame.Characters.Text = 住所;                  //宛先住所
-            宛名面.Shapes("宛先氏名1").TextFrame.Characters.Text = 氏名;                             //宛先氏名1
-            宛名面.Shapes("宛先氏名2").TextFrame.Characters.Text = 氏名2;                                           //宛先氏名2
-            宛名面.Shapes("差出人氏名").TextFrame.Characters.Text = 差出人氏名;                         //差出人氏名
-            宛名面.Shapes("差出人住所").TextFrame.Characters.Text = 差出人住所;       //差出人住所
+            //dynamic printOut = 宛名面.Range["A1:D32"].PrintOut;
 
-            //dynamic printOut = 宛名面2.Range["A1:L3"].PrintOut;
-            dynamic printOut = 宛名面.Range["A1:D32"].PrintOut;
-
-            //// Excelのクローズ
-            //xlBook.Saved = true;
-            //xlBook.Close();
-            //xlApp.Quit();
-            //System.Runtime.InteropServices.Marshal.ReleaseComObject((object)xlBook);
-            //System.Runtime.InteropServices.Marshal.ReleaseComObject((object)xlBooks);
-            //System.Runtime.InteropServices.Marshal.ReleaseComObject((object)xlApp);
         }
 
 
@@ -367,35 +260,9 @@ namespace 口酒井農業水利組合郵送会員住所録
         private void 差出人変更btn_Click(object sender, EventArgs e)
         {
 
-            //xlApp = new Excel.Application();
-            //xlBooks = xlApp.Workbooks;
-            //xlBook = xlBooks.Open(水利関係住所録WB);
-
-            //MessageBox.Show(xlApp.name);
-            ////MessageBox.Show(xlBooks.thisworkbook.name);
-            //MessageBox.Show(xlBook.name);
-
-            ////シートを選択
-            var Sashidashinin = xlBook.Sheets["差出人"];
-
-            //if (Sashidashinin.Range["A2"].value = 差出人Box.Text)
-
-            Sashidashinin.Range["A2"].value = 差出人Box.Text;
-            Sashidashinin.Range["B2"].value = 差出人住所Box.Text;
-
-            //上書き保存
-            xlBook.Save();
 
             MessageBox.Show("変更を保存しました");
 
-            // Excelのクローズ
-            //xlBook.Close();
-            //xlApp.Quit();
-
-            ////使用したCOMオブジェクトを解放その２
-            //System.Runtime.InteropServices.Marshal.ReleaseComObject((object)xlBook);
-            //System.Runtime.InteropServices.Marshal.ReleaseComObject((object)xlBooks);
-            //System.Runtime.InteropServices.Marshal.ReleaseComObject((object)xlApp);
 
         }
 
@@ -413,11 +280,6 @@ namespace 口酒井農業水利組合郵送会員住所録
                 fs = new 住所氏名編集Form();
                 fs.formMain = this;
 
-            // Excelのクローズ
-                xlBook.Saved = true;
-                xlBook.Close();
-                //xlApp.Quit();
-
                 ListViewItem itemx = listView1.SelectedItems[0];
 
                 string ID = itemx.SubItems[0].Text;
@@ -432,11 +294,6 @@ namespace 口酒井農業水利組合郵送会員住所録
 
                 //住所氏名変更Formを呼び出す
                 fs.ShowDialog();
-
-
-            //xlApp = Activator.CreateInstance(Type.GetTypeFromProgID("Excel.Application"));
-            //xlBooks = xlApp.Workbooks;
-            xlBook = xlBooks.Open(水利関係住所録WB);
 
 
             if (Lvflag == "追加")
@@ -493,22 +350,6 @@ namespace 口酒井農業水利組合郵送会員住所録
 
         private void 住所録リストForm_FormClosing(object sender, FormClosingEventArgs e)
         {
-            //// 使用したCOMオブジェクトを解放
-            //System.Runtime.InteropServices.Marshal.ReleaseComObject(TableRange);
-            //System.Runtime.InteropServices.Marshal.ReleaseComObject(sheet);
-            //System.Runtime.InteropServices.Marshal.ReleaseComObject(差出人);
-            //System.Runtime.InteropServices.Marshal.ReleaseComObject(sashidasinin);
-
-            //Excelのクローズ
-            xlBook.Saved = true;
-            xlBook.Close();
-            xlBooks.Close();
-            xlApp.Quit();
-
-            //// 使用したCOMオブジェクトを解放その２
-            System.Runtime.InteropServices.Marshal.ReleaseComObject((object)xlBook);
-            System.Runtime.InteropServices.Marshal.ReleaseComObject((object)xlBooks);
-            System.Runtime.InteropServices.Marshal.ReleaseComObject((object)xlApp);
         }
 
  
